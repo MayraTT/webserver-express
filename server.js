cbd0bd03fb6bbc23f2270c776bea9d2038f0bcd0
MayraNode <mayra.lopez@fumired.com.mx>
2020-04-30 09:55:37 -0500

    primer commit!

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parcials');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'mayra',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {});
+});
+
+// app.get('/', (req, res) => {
+//     // res.send('Hola mundo');
+//     let salida = {
+//         nombre: 'Mayra',
+//         edad: '2.',
+//         url: req.url
+//     }
+//     res.send(salida);
+// });
+
+
+// app.get('/data', (req, res) => {
+
+//     res.send('Hola Data');
+// });
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones de puerto ${port}`);
+});
